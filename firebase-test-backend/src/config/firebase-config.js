519ad11ff9220a42ed5012a5b777f79ba073c217
Leanne251 <leannesmith251@gmail.com>
2022-04-19 18:13:15 +0100

    changed config vars

--- firebase-test-backend/src/config/firebase-config.js
+++ firebase-test-backend/src/config/firebase-config.js
@@ -1,8 +1,16 @@
+// import admin from 'firebase-admin';
+// // import serviceAccount from './serviceAccount.json';
+
+// // admin.initializeApp({
+// // 	credential: admin.credential.cert(serviceAccount)
+// // });
+
+// export default admin;
+
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccount.json';
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
+	credential: admin.credential.cert(JSON.parse(process.env.serviceAccountKey))
 });
 
 export default admin;
