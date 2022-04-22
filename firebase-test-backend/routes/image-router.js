786b4b396e8394b881d31c1daf6decc6589aed15
Leanne251 <leannesmith251@gmail.com>
2022-04-22 17:14:44 +0100

    del

--- firebase-test-backend/routes/image-router.js
+++ firebase-test-backend/routes/image-router.js
@@ -4,16 +4,16 @@ import { cloud } from '../src/config/firebase-config.js';
 const imageRouter = express.Router();
 console.log(cloud);
 
-imageRouter.post('/', async (req, res) => {
+imageRouter.post('/', (req, res) => {
 	try {
 		const fileStr = req.body.data;
 		console.log('fileStr', fileStr);
-		const uploadedResponse = await cloud.uploader.upload(fileStr, {});
-		console.log('fileStr', fileStr);
-		console.log('uploadedresponse', uploadedResponse);
-		res.json({
-			msg: 'YAY'
-		});
+		// const uploadedResponse = await cloud.uploader.upload(fileStr, {});
+		// console.log('fileStr', fileStr);
+		// console.log('uploadedresponse', uploadedResponse);
+		// res.json({
+		// 	msg: 'YAY'
+		// });
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ err: 'something went wrong' });
