36b066b6a36ad0762668613031e647420c00346f
Leanne251 <leannesmith251@gmail.com>
2022-04-22 17:24:46 +0100

    changed variable

--- firebase-test-backend/routes/image-router.js
+++ firebase-test-backend/routes/image-router.js
@@ -4,16 +4,16 @@ import { cloud } from '../src/config/firebase-config.js';
 const imageRouter = express.Router();
 console.log(cloud);
 
-imageRouter.post('/', (req, res) => {
+imageRouter.post('/', async (req, res) => {
 	try {
-		const fileStr = req.body.data;
+		const fileStr = req.body;
 		console.log('fileStr', fileStr);
-		// const uploadedResponse = await cloud.uploader.upload(fileStr, {});
-		// console.log('fileStr', fileStr);
-		// console.log('uploadedresponse', uploadedResponse);
-		// res.json({
-		// 	msg: 'YAY'
-		// });
+		const uploadedResponse = await cloud.uploader.upload(fileStr, {});
+		console.log('fileStr', fileStr);
+		console.log('uploadedresponse', uploadedResponse);
+		res.json({
+			msg: 'YAY'
+		});
 	} catch (err) {
 		console.error(err);
 		res.status(500).json({ err: 'something went wrong' });
