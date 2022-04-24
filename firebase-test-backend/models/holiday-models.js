2c61f37969e0a74c7d911b210ea649f755f7f094
Leanne251 <leannesmith251@gmail.com>
2022-04-24 11:11:45 +0100

    changed image url

--- firebase-test-backend/models/holiday-models.js
+++ firebase-test-backend/models/holiday-models.js
@@ -26,11 +26,11 @@ export async function usersOwnHolidays(userID) {
 }
 
 export async function addAHoliday({ user_id, destination, style, hotel, image }) {
-	const uploadedResponse = await cloudinary.uploader.upload(image, {
-		upload_preset: 'Holiday_Project'
-	});
-	const imageURL = uploadedResponse.url;
-	console.log('image URL', imageURL);
+	// const uploadedResponse = await cloudinary.uploader.upload(image, {
+	// 	upload_preset: 'Holiday_Project'
+	// });
+	// const imageURL = uploadedResponse.url;
+	// console.log('image URL', imageURL);
 	const result = await db.query(
 		`INSERT INTO holidays (user_id, destination, style, hotel, image_URL) VALUES ($1, $2, $3, $4, $5) RETURNING destination;`,
 		[ user_id, destination, style, hotel, imageURL ]
